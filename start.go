0cfb244ce31ff32b5da4a326cbd0708e25929bad
zhangsan33 <8546260+zhangsan3356@user.noreply.gitee.com>
2023-02-07 15:54:07 +0800

    put a function to get a connection per invoking

---
+++ start.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GRTheory/my-model/ent"
+)
+
+func GetClient(username, password, host, port, database string) (*ent.Client, error) {
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True", username, password, host, port, database)
+	client, err := ent.Open("mysql", connection)
+	if err != nil {
+		return nil, err
+	}
+	err = client.Schema.Create(context.Background())
+	return client, err
+}
